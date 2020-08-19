158844d25500dd99139780bed5e19cee1c47487d
Hannan Gigih Prastawa <hannanxp@gmail.com>
2020-08-19 17:55:03 +0700

    test hello

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <p>This is example from ilmucoding.com</p>
+    <div class="card">
+        <div class="card-header">
+        asdfSDF
+        </div>
+        <div class="card-body">
+        asdfSDF
+        </div>
+    </div>
+</body>
+</html>
