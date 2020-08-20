82f69358526b3905812ed86fa668d646e8b56e30
Hannan Gigih Prastawa <hannanxp@gmail.com>
2020-08-20 08:46:04 +0700

    refine base blade

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Laravel 7 & MySQL CRUD Tutorial</title>
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
-</head>
-<body>
-  <div class="container">
-    @yield('main')
-  </div>
-  <script src="{{ asset('js/app.js') }}" type="text/js"></script>
-</body>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sehatqu Test</title>
+    
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      @yield('main')
+    </div>
+  </body>
 </html>
